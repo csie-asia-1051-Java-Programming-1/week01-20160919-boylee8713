e928d77b749910803b0ce3a9d7a49e53ed0cb9b8
boylee8713 <105021061@live.asia.edu.tw>
2016-10-24 14:44:44 +0800

    交

---
+++ week01_20160919/src/hw/hw01_105021061.java
@@ -0,0 +1,16 @@
+package hw;
+import java.util.Scanner;
+public class hw01_105021061 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("¿é¤Jn");
+		int n =scn.nextInt();
+		
+		Math.ceil(Math.log(n)/Math.log(2));
+		System.out.println(Math.ceil(Math.log(n)/Math.log(2)));
+
+	}
+
+}
---
+++ week01_20160919/src/hw/hw04_105021061.java
@@ -0,0 +1,15 @@
+package hw;
+ import java.util.Scanner;
+public class hw04_105021061 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("¿é¤Ja,b");
+		Scanner scn = new Scanner(System.in);
+		double a = scn.nextDouble();
+		double b = scn.nextDouble();
+		double c = Math.pow(a,1/b);
+    System.out.println(c);
+	}
+
+}
